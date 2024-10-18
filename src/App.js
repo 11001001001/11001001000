ecfa2c6a62e971146190aca9d091e507b31f8551
home <home@192.168.1.2>
2024-10-18 23:39:32 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   const [registered, setRegistered] = useState(false);
   const [isLoading, setIsLoading] = useState(true); // Состояние загрузки
 
-
   function decodePromoCode(promoCode) {
     let decoded = '';
     
@@ -25,12 +24,12 @@ function App() {
   // Fetch data from cloudStorage (balance, registration status)
   useEffect(() => {
     const getInitialData = () => {
-      window.Telegram.WebApp.CloudStorage.getItems(['balanceC', 'registered2'], (error, result) => {
+      window.Telegram.WebApp.CloudStorage.getItems(['balanceC', 'registered3'], (error, result) => {
         if (error) {
           console.error('Failed to get initial data from cloud storage:', error);
         } else {
           const initialBalance = result.balanceC ? parseInt(result.balanceC, 10) : 0;
-          const registeredCheck = result.registered2 ? result.registered2 === 'true' : false;
+          const registeredCheck = result.registered3 ? result.registered3 === 'true' : false;
           setBalance(initialBalance);
           setRegistered(registeredCheck);
         }
@@ -39,94 +38,105 @@ function App() {
     getInitialData();
   }, []);
 
-  // Register user if not registered
+  // Check if user exists in the database
   useEffect(() => {
     const platform = window.Telegram.WebApp.platform;
     if (platform !== "ios" && platform !== "android") {
       window.location.replace('https://hdr2029.github.io/monofacture/');    
     }
-    const checkAndRegisterUser = async () => {
-      if (registered) {
-        // User is already registered, update the balance
-        const user = window.Telegram.WebApp.initDataUnsafe.user;
-        if (user) {
-          const { id: user_id } = user;
-          try {
-            const response = await fetch(`https://bye-b7c975e7a8fb.herokuapp.com/api/update-balance/${user_id}/`, {
-              method: 'PUT',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ balance })
+
+    const checkUserExists = async () => {
+      const user = window.Telegram.WebApp.initDataUnsafe.user;
+
+      if (user) {
+        const { id: user_id } = user;
+
+        try {
+          const response = await fetch(`https://bye-b7c975e7a8fb.herokuapp.com/api/check-user/${user_id}/`);
+
+          if (response.ok) {
+            const data = await response.json();
+            console.log('User found:', data);
+            setBalance(parseInt(data.balance, 10));
+
+            // User exists, mark as registered in cloudStorage
+            window.Telegram.WebApp.CloudStorage.setItem('registered3', 'true', (error) => {
+              if (error) {
+                console.error('Failed to update registered3 in cloud storage:', error);
+              }
             });
+            setRegistered(true);
+            setIsLoading(false);
 
-            if (response.ok) {
-              console.log('Balance updated successfully');
-              setIsLoading(false)
-            } else {
-              console.error('Error updating balance:', await response.json());
-              setIsLoading(false)
-
-            }
-          } catch (error) {
-            console.error('Request failed:', error);
-            setIsLoading(false)
-
+          } else if (response.status === 404) {
+            // User not found, register the user
+            registerUser();
+          } else {
+            console.error('Error checking user existence:', await response.json());
+            setIsLoading(false);
           }
-        }
-      } else {
-        // Register the user
-        const user = window.Telegram.WebApp.initDataUnsafe.user;
-        const promoCode = window.Telegram.WebApp.initDataUnsafe.start_param;
-        console.log('Promo code received:', promoCode);
-
-        if (user) {
-          const { username, first_name, last_name, id: user_id } = user;
-          const name = first_name || last_name;
-
-
-          const newUser = {
-            username: username || '',
-            name: name || '',
-            user_id: String(user_id),
-            balance: "0",
-            photo_url: '',
-            promo_code: decodePromoCode(promoCode) || ''
-          };
-
-          console.log("Data being sent to the server:", newUser);
-
-          try {
-            const response = await fetch('https://bye-b7c975e7a8fb.herokuapp.com/api/create-user/', {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify(newUser),
-            });
-
-            if (response.ok) {
-              // Save to cloudStorage instead of localStorage
-              window.Telegram.WebApp.CloudStorage.setItem('registered2', 'true', (error) => {
-                if (error) {
-                  console.error('Failed to update balance in cloud storage:', error);
-                }
-              });
-              setIsLoading(false)
-
-              console.log('User registered and stored in cloudStorage');
-            } else {
-              console.error('Error during registration:', await response.json());
-              setIsLoading(false)
-
-            }
-          } catch (error) {
-            console.error('Request failed:', error);
-            setIsLoading(false)
-
-          }
+        } catch (error) {
+          console.error('Request failed:', error);
+          setIsLoading(false);
         }
       }
     };
 
-    checkAndRegisterUser();
-  }, [registered, balance]);
+    const registerUser = async () => {
+      const user = window.Telegram.WebApp.initDataUnsafe.user;
+      const promoCode = window.Telegram.WebApp.initDataUnsafe.start_param;
+      console.log('Promo code received:', promoCode);
+
+      if (user) {
+        const { username, first_name, last_name, id: user_id } = user;
+        const name = first_name || last_name;
+
+        const newUser = {
+          username: username || '',
+          name: name || '',
+          user_id: String(user_id),
+          balance: "0",
+          photo_url: '',
+          promo_code: decodePromoCode(promoCode) || ''
+        };
+
+        console.log("Data being sent to the server:", newUser);
+
+        try {
+          const response = await fetch('https://bye-b7c975e7a8fb.herokuapp.com/api/create-user/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newUser),
+          });
+
+          if (response.ok) {
+            console.log('User registered successfully');
+
+            // Mark user as registered in cloudStorage
+            window.Telegram.WebApp.CloudStorage.setItem('registered3', 'true', (error) => {
+              if (error) {
+                console.error('Failed to update registered3 in cloud storage:', error);
+              }
+            });
+            setRegistered(true);
+            setIsLoading(false);
+          } else {
+            console.error('Error during registration:', await response.json());
+            setIsLoading(false);
+          }
+        } catch (error) {
+          console.error('Request failed:', error);
+          setIsLoading(false);
+        }
+      }
+    };
+
+    if (!registered) {
+      checkUserExists();
+    } else {
+      setIsLoading(false);
+    }
+  }, [registered]);
 
   const triggerHapticFeedback = () => {
     if (window.Telegram.WebApp) {
@@ -140,36 +150,37 @@ function App() {
 
   return (
     <>
-    {isLoading ?
-    ( <div className='spinner-container' style={{height:'100vh'}}>
-      <div className="spinner">
-        <img src="https://i.ibb.co/GnPQ1jd/IMG-2102.png" alt="loading" className="spinner-image" />
-      </div>
-    </div>)
-    :
-    (<Router basename="/11001001000">
-      <div className="app">
-        <Routes >
-          <Route path="/" element={<HomePage />} exact />
-          <Route path="/friends" element={<FriendsPage />} exact />
-          <Route path="/leaders" element={<LeaderPage />} exact />
-        </Routes>
-        <div className="fixed-buttons">
-          <Link to="/" className="nav-link" onClick={triggerHapticFeedback}>
-            <img src="https://img.icons8.com/?size=100&id=LiGfY7DuMBd4&format=png&color=FFFFFF" alt="Home" className="nav-icon" />
-            <span>Mining</span>
-          </Link>
-          <Link to="/leaders" className="nav-link" onClick={triggerHapticFeedback}>
-            <img src="https://img.icons8.com/?size=100&id=2I9Hz4gixhSZ&format=png&color=FFFFFF" alt="Page 1" className="nav-icon" />
-            <span>Leaderboard</span>
-          </Link>
-          <Link to="/friends" className="nav-link" onClick={triggerHapticFeedback}>
-            <img src="https://img.icons8.com/?size=100&id=dyY8SQ06ZX9R&format=png&color=FFFFFF" alt="Page 2" className="nav-icon" />
-            <span>Friends</span>
-          </Link>
+      {isLoading ? (
+        <div className='spinner-container' style={{height:'100vh'}}>
+          <div className="spinner">
+            <img src="https://i.ibb.co/GnPQ1jd/IMG-2102.png" alt="loading" className="spinner-image" />
+          </div>
         </div>
-      </div>
-    </Router>)}
+      ) : (
+        <Router basename="/11001001000">
+          <div className="app">
+            <Routes>
+              <Route path="/" element={<HomePage />} exact />
+              <Route path="/friends" element={<FriendsPage />} exact />
+              <Route path="/leaders" element={<LeaderPage />} exact />
+            </Routes>
+            <div className="fixed-buttons">
+              <Link to="/" className="nav-link" onClick={triggerHapticFeedback}>
+                <img src="https://img.icons8.com/?size=100&id=LiGfY7DuMBd4&format=png&color=FFFFFF" alt="Home" className="nav-icon" />
+                <span>Mining</span>
+              </Link>
+              <Link to="/leaders" className="nav-link" onClick={triggerHapticFeedback}>
+                <img src="https://img.icons8.com/?size=100&id=2I9Hz4gixhSZ&format=png&color=FFFFFF" alt="Page 1" className="nav-icon" />
+                <span>Leaderboard</span>
+              </Link>
+              <Link to="/friends" className="nav-link" onClick={triggerHapticFeedback}>
+                <img src="https://img.icons8.com/?size=100&id=dyY8SQ06ZX9R&format=png&color=FFFFFF" alt="Page 2" className="nav-icon" />
+                <span>Friends</span>
+              </Link>
+            </div>
+          </div>
+        </Router>
+      )}
     </>
   );
 }
