7a8081ae7eaf4e11aa51d55b11cae6cf80f91015
home <home@192.168.1.3>
2024-10-22 08:27:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -66,13 +66,6 @@ function App() {
               }
             });
 
-            if ( data.balance > balance) {
-              window.Telegram.WebApp.CloudStorage.setItem('balanceC', Math.floor(data.balance).toString(), (error) => {
-                if (error) {
-                  console.error('Failed to update registered3 in cloud storage:', error);
-                }
-              });
-            }
             setRegistered(true);
             setIsLoading(false);
 
